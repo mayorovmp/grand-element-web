c3e9cc861c1dea5f800b94f26cd59d1d47b520ab
mayorovmp <mayorovmp@yandex.ru>
2019-12-19 20:24:08 +0500

    register suppliers in catalogs

--- src/app/catalogs/catalogs.component.ts
+++ src/app/catalogs/catalogs.component.ts
@@ -7,8 +7,12 @@ import { Catalog } from './models/Catalog';
   styleUrls: ['./catalogs.component.css']
 })
 export class CatalogsComponent implements OnInit {
-  catalogs: Catalog[] = [new Catalog('Клиенты', ['clients']), new Catalog('Категории машин', ['car-category']),
-  new Catalog('Автомобили', ['car'])];
+  catalogs: Catalog[] = [
+    new Catalog('Клиенты', ['clients']),
+    new Catalog('Категории машин', ['car-category']),
+    new Catalog('Автомобили', ['car']),
+    new Catalog('Поставщики', ['suppliers'])
+  ];
   constructor() { }
 
   ngOnInit() {
