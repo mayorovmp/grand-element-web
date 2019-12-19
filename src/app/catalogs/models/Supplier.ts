48fc2f70dca9dc5898345dd96961a63b152371b0
mayorovmp <mayorovmp@yandex.ru>
2019-12-19 21:15:24 +0500

    edit supplierComp

--- src/app/catalogs/models/Supplier.ts
+++ src/app/catalogs/models/Supplier.ts
@@ -1,5 +1,8 @@
+import { Product } from './Product';
+
 export class Supplier {
   legalEntity: string; // Юр. лицо
   address: string;
   name: string;
+  products: string[];
 }
--- src/app/catalogs/suppliers/suppliers.component.ts
+++ src/app/catalogs/suppliers/suppliers.component.ts
@@ -10,11 +10,12 @@ import { AddSupplierComponent } from './add-supplier/add-supplier.component';
 })
 export class SuppliersComponent implements OnInit {
 
-  suppliers: Supplier[] = [{ address: 'Ленина 48', name: 'Рога', legalEntity: 'ООО Рога и копыта' },
-  { address: 'Копейское шоссе 2', name: 'Топ1', legalEntity: 'ИП Банников' },
-  { address: 'Энергетиков 418', name: 'Болид', legalEntity: 'ИП Прон' },
-  { address: 'Шорса 48', name: 'ДВР', legalEntity: 'ЗАО ЭЛЕМЕНТ' },
-  { address: 'Агалакова 38', name: 'НаноМир', legalEntity: 'ООО ЗХ и ХЗ' }];
+  suppliers: Supplier[] = [
+    { address: 'Ленина 48', name: 'Рога', legalEntity: 'ООО Рога и копыта', products: ['песок', 'щебень', 'камни'] },
+    { address: 'Копейское шоссе 2', name: 'Топ1', legalEntity: 'ИП Банников', products: ['асбест', 'щебень', 'камни'] },
+    { address: 'Энергетиков 418', name: 'Болид', legalEntity: 'ИП Прон', products: ['щебень', 'мрамор'] },
+    { address: 'Шорса 48', name: 'ДВР', legalEntity: 'ЗАО ЭЛЕМЕНТ', products: ['гранит', 'известь'] },
+    { address: 'Агалакова 38', name: 'НаноМир', legalEntity: 'ООО ЗХ и ХЗ', products: ['песок'] }];
 
   newSupplier: Supplier;
 
