257f3923b2c03af63e8fa412bdf9fde021292edf
mayorovmp <mayorovmp@yandex.ru>
2020-01-11 16:44:19 +0500

    fix navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
+import { Catalog } from '../catalogs/models/Catalog';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +9,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  catalogs: Catalog[] = [
+    new Catalog('Клиенты', ['clients']),
+    new Catalog('Категории машин', ['car-category']),
+    new Catalog('Перевозчики', ['car']),
+    new Catalog('Товары', ['product']),
+    new Catalog('Поставщики', ['suppliers'])
+  ];
 
   constructor(public auth: AuthService, private router: Router, ) { }
 
