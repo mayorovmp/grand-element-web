33ef962aa22d8784286f8a957d053e7a6206889a
mayorovmp <mayorovmp@yandex.ru>
2020-02-01 15:43:06 +0500

    fix/change dev server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:5000'
-  // baseUrl: 'https://grand-element.azurewebsites.net'
-  // baseUrl: 'http://127.0.0.1:5555'
+  // baseUrl: 'http://localhost:5000'
+  baseUrl: 'https://grand-element.azurewebsites.net'
 };
 
 /*
