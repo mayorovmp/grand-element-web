01ce9ffc272d6008da1a79b248a32e38781703d8
alexey <pronin7work@gmail.com>
2020-02-02 18:24:38 +0500

    feature/Orders: change filenames

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -10,8 +10,8 @@ import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-orders',
-  templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  templateUrl: './orders.html',
+  styleUrls: ['./styles.css']
 })
 export class OrdersComponent implements OnInit {
   pickedDay = new Date(Date.now());
