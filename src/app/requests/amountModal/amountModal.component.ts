c23c80b1cd0a0cebacc46b7b4479175499a919a1
alexey <pronin7work@gmail.com>
2020-08-29 11:59:08 +0500

    amount modal changes

--- src/app/requests/amountModal/amountModal.component.ts
+++ src/app/requests/amountModal/amountModal.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgxSmartModalService } from 'ngx-smart-modal';
+import { Request } from 'src/app/models/Request';
+import { Car } from 'src/app/models/Car';
+import { HttpService as CarHttp } from 'src/app/catalogs/car/http.service';
 
 @Component({
   selector: 'app-modal-amount',
@@ -10,30 +13,46 @@ export class AmountModalComponent implements OnInit {
 
   @Output() changed = new EventEmitter<any>();
 
-  modalTitle = 'Подтвердить действие';
-  modalActionTitle = 'подтвердить';
-  modalBtnActionColor = 'gray';
-  modalAction: any = null;
+  request: Request = new Request();
+  cars: Car[] = [];
+  carOwnerText = '';
+  carListVisible = false;
+  parentAmount = 0;
 
-  constructor( private ngxSmartModalService: NgxSmartModalService) { }
+  constructor(
+    private ngxSmartModalService: NgxSmartModalService,
+    private carHttp: CarHttp
+  ) { }
 
   ngOnInit() { }
 
   async onOpen() {
-    const transferred = this.ngxSmartModalService.getModalData('confirmModal');
+    this.reset();
+    this.carHttp.getCars().toPromise().then(cars => this.cars = cars);
+    const transferred = this.ngxSmartModalService.getModalData('amountModal');
     if (transferred) {
-      const { title, btnAction, btnActionName, btnActionColor } = transferred;
-      this.modalTitle = title;
-      this.modalAction = () => {
-        btnAction();
-        this.onClose();
-      };
-      this.modalActionTitle = btnActionName;
-      this.modalBtnActionColor = btnActionColor;
+      const { req } = transferred;
+      this.request = req;
+      this.parentAmount = req.amount;
     }
+    console.log('this.req', this.request)
+  }
+  selectCarOwner(car: Car) {
+    this.carListVisible = false;
+    if (car.owner) {
+      this.carOwnerText = car.owner;
+    }
+    this.request.car = car;
+  }
+  reset() {
+    this.request = new Request();
+    this.cars = [];
+    this.carOwnerText = '';
+    this.carListVisible = false;
+    this.parentAmount = 0;
   }
   onClose() {
     this.changed.emit();
-    this.ngxSmartModalService.close('confirmModal');
+    this.ngxSmartModalService.close('amountModal');
   }
 }
