40ac4a592829c17fe57d4f56e061873d483088ad
Alexey <alex7pronin@gmail.com>
2021-04-14 21:29:52 +0500

    ver-1.1: implement supplier and carOwner input in confirm modal

--- src/app/requests/confirmCompleteReqModal/amountModal/confirm-complete-req-modal.component.ts
+++ src/app/requests/confirmCompleteReqModal/amountModal/confirm-complete-req-modal.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgxSmartModalService } from 'ngx-smart-modal';
-import { Request } from '@models/Request';
-import { Car } from '@models/Car';
-import { HttpService as CarHttp } from 'src/app/catalogs/car/http.service';
-import { HttpService as ReqService } from 'src/app/requests/http.service';
 import { ToastrService } from 'ngx-toastr';
+import { SupplierEditorComponent } from 'src/app/catalogs/suppliers/editor/editor.component';
+import { CarEditorComponent } from 'src/app/catalogs/car/editor/editor.component';
+import { HttpService as ReqService } from 'src/app/requests/http.service';
+import { HttpService as SupplierHttp } from 'src/app/catalogs/suppliers/http.service';
+import { HttpService as CarHttp } from 'src/app/catalogs/car/http.service';
+import { Request } from '@models/Request';
+import { Supplier } from '@models/Supplier';
+import { Car } from '@models/Car';
 
 @Component({
   selector: 'app-confirm-complete-req-modal-amount',
@@ -16,12 +20,20 @@ export class ConfirmCompleteReqModalComponent implements OnInit {
 
   @Output() changed = new EventEmitter<any>();
 
-  request: Request = new Request();
   constructor(
     private ngxSmartModalService: NgxSmartModalService,
     private reqService: ReqService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private supplierHttp: SupplierHttp,
+    private carHttp: CarHttp
   ) {}
+
+  request: Request = new Request();
+  cars: Car[] = [];
+  suppliers: Supplier[] = [];
+  isSupplierEmpty =  false;
+  isCarOwnerEmpty = false;
+
 
   ngOnInit() {}
 
@@ -32,10 +44,28 @@ export class ConfirmCompleteReqModalComponent implements OnInit {
     );
     if (transferred) {
       this.request = transferred;
+      console.log('this.request', this.request);
+      if (!this.request.supplier) {
+        this.isSupplierEmpty = true;
+        this.supplierHttp
+        .getSuppliers()
+        .toPromise()
+        .then((x) => (this.suppliers = x))
+      }
+      if (!this.request.car) {
+        this.isCarOwnerEmpty = true;
+        this.carHttp
+        .getCars()
+        .toPromise()
+        .then((x) => (this.cars = x))
+      }
     }
   }
+
   reset() {
     this.request = new Request();
+    this.isSupplierEmpty =  false;
+    this.isCarOwnerEmpty = false;
   }
   onClose() {
     this.changed.emit();
@@ -48,7 +78,14 @@ export class ConfirmCompleteReqModalComponent implements OnInit {
   async apply() {
     if (!this.request.amount) {
       this.toastr.error('Заполните объем');
-    } else {
+    } 
+    if (!this.request.supplier) {
+      this.toastr.error('Выберите перевозчика');
+    }
+    if (!this.request.car) {
+      this.toastr.error('Выберите поставщика');
+    }
+    if (this.request.amount && this.request.supplier && this.request.car){
       await this.reqService.edit(this.request).toPromise();
       if (this.request.id) {
         await this.reqService.setStatus(this.request.id, 2).toPromise();
@@ -57,5 +94,29 @@ export class ConfirmCompleteReqModalComponent implements OnInit {
         ConfirmCompleteReqModalComponent.MODAL_NAME
       );
     }
+  }
+  addSupplier() {
+    this.ngxSmartModalService
+      .getModal(SupplierEditorComponent.MODAL_NAME)
+      .open();
+  }
+  addCarOwner() {
+    this.ngxSmartModalService.getModal(CarEditorComponent.MODAL_NAME).open();
+  }
+  onCarOwnerAdd(car: Car) {
+    this.cars = [];
+    this.carHttp.getCars().subscribe((x) => {
+      this.cars = x;
+      this.request.car = car;
+    });
+  }
+  onSupplierAdd(supplier: Supplier) {
+    this.supplierHttp.getSuppliers().subscribe((x) => {
+      this.suppliers = x;
+      this.request.supplier = supplier;
+      if (supplier.products.length) {
+        this.request.product = supplier.products[0];
+      }
+    });
   }
 }
